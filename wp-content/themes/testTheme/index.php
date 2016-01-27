02087a7db86ee254edf3ecfe8a9109bcfd6517a8
jobcrackerbah <erik@critiqueit.com>
2016-01-27 00:22:58 -0800

    Adds title to page

--- wp-content/themes/testTheme/index.php
+++ wp-content/themes/testTheme/index.php
@@ -1,8 +1,15 @@
 <?php get_header(); ?>
 
-	<?php if( have_posts() ):
+	<?php if( have_posts() ): ?>
+		<header>
+			<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+		</header>
+		<?php if ( is_home() && ! is_front_page() ) : ?>
+			
+		<?php endif; ?>
+		
 
-		while( have_posts() ): the_post(); ?>
+		<? while( have_posts() ): the_post(); ?>
 
 			<p><?php the_content(); ?></p>
 
