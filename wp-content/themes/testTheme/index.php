d406267f7b3257b88f1c638d9a08f8ee830ac48d
jobcrackerbah <erik@critiqueit.com>
2016-01-27 00:33:14 -0800

    Adds title to menu navigation pages

--- wp-content/themes/testTheme/index.php
+++ wp-content/themes/testTheme/index.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
 	<?php if( have_posts() ): ?>
-		<header>
-			<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-		</header>
+			<h1 class="page-title screen-reader-text">
+				<?php single_post_title(); ?>
+			</h1>
+
 		<?php if ( is_home() && ! is_front_page() ) : ?>
 			
 		<?php endif; ?>
