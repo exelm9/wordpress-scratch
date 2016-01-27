c26f0ee4983a11f679f1cc43bb2f5ddb5468a806
jobcrackerbah <erik@critiqueit.com>
2016-01-26 23:25:01 -0800

    Adds index, header, and footer code

---
+++ wp-content/themes/testTheme/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Test Theme</title>
+	<!-- connects wordpress hooks -->
+		<?php wp_head(); ?>
+	
+</head>
+
+<body <?php body_class(); ?>>
+<header id="masthead" class="site-header" role="banner">
+
+	<div id="navbar" class="navbar">
+		<?php wp_nav_menu(array('theme_location'=>'main')); ?>
+	</div><!-- #navbar -->
+	<!-- grabs first menu available.  If want to preload other menus, have to understand paramters passed -->
+	
+</header>
